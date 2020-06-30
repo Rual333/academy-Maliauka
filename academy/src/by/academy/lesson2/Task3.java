c4fc8ffd136c4faae71507095baa4cf47c2bcd96
Rual333 <Chromer.00@gmail.com>
2020-06-30 22:01:03 +0300

    Add Lesson 2 Tasks: 2,3,4,5.

---
+++ academy/src/by/academy/lesson2/Task3.java
@@ -0,0 +1,21 @@
+/*
+ * Task3
+ * 
+ * Creates random number from 0 to Integer.MAX_VALUE and prints if its even or odd
+ * 
+ * Author: Ivan Maliauka
+ * 
+ */
+
+package by.academy.lesson2;
+
+public class Task3 {
+	public static void main(String[] args) {
+		int number = (int) (Math.random() * Integer.MAX_VALUE);
+		if (number % 2 == 0) {
+			System.out.println("Created number " + number + " is even");
+		} else {
+			System.out.println("Created number " + number + " is odd");
+		}
+	}
+}
