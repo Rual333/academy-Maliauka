723f6b6ed4d675e073ee4e9767a9306eac92cda3
Rual333 <Chromer.00@gmail.com>
2020-07-02 00:38:51 +0300

    Add lesson 3 Tasks 1-5, 1-4.

---
+++ src/by/academy/lesson3/MassTask3.java
@@ -0,0 +1,20 @@
+package by.academy.lesson3;
+
+import java.util.Random;
+
+public class MassTask3 {
+	public static void main(String... strings) {
+		int[] arr = new int[15];
+		Random rand = new Random();
+		int kol = 0;
+		for (int a : arr) {
+			a = rand.nextInt(10);
+			System.out.print(a + " ");
+			if (a % 2 == 0) {
+				kol++;
+			}
+		}
+		System.out.println();
+		System.out.println(kol);
+	}
+}
